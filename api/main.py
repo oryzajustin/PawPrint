5106d9dd0be122de2827369cf7fce3aef0879556
JJ <jj@jj.ai>
2017-10-21 16:16:02 -0400

    Flask docker working

---
+++ api/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os, time
+import sys
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+print("starting program...")
+
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0')
