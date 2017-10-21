033f3d4521b94b2c15396afddc095bc9f225f7bf
Justin Koh <jkoh8@uwo.ca>
2017-10-21 02:20:21 -0400

    init

--- twilio/credentials.py
+++
@@ -1,6 +0,0 @@
-#Account SID from twilio.com/console
-account_sid = "ACbaf76a0ab764a2f84e6940d324bd2e2d"
-#Auth Token from twilio.com/console
-auth_token  = "167fddad53228fd06dbc2ca0aacc0bb9"
-my_number = "+13474595013"
-twilio_number = "+19029052328"
