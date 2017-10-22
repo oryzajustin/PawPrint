d41a52681da36e6167f805c77854e0ca213d83a3
JJ <jj@jj.ai>
2017-10-21 22:30:22 -0400

    found animal working

--- api/dbase.py
+++ api/dbase.py
@@ -21,6 +21,11 @@ class DBase:
 		)
 		self.cursor = self.db.cursor()
 
+	def results(self, cursor):
+		columns = cursor.description 
+		result = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
+		return result
+
 	def __enter__(self):
 		return DBase()
 
@@ -28,15 +33,17 @@ class DBase:
 		if self.db:
 			self.db.close()
 
-	def getOne(self,sql):
+	def getOne(self, sql):
 		self.cursor.execute(sql)
 		return self.cursor.fetchone()
 
 	def getAll(self, sql):
 		self.cursor.execute(sql)
-		columns = self.cursor.description 
-		result = [{columns[index][0]:column for index, column in enumerate(value)} for value in self.cursor.fetchall()]
-		return result
+		return self.results(self.cursor)
+		#JJ: make sure this works
+		#columns = self.cursor.description 
+		#result = [{columns[index][0]:column for index, column in enumerate(value)} for value in self.cursor.fetchall()]
+		#return result
 		#return self.cursor.fetchall()
 
 	def rows(self):
@@ -45,4 +52,32 @@ class DBase:
 	def getAllCameras(self):
 		sql = 'SELECT * FROM camera'
 		return self.getAll(sql)
+
+	def getCameraByUrl(self, url):
+		sql = 'SELECT id FROM camera WHERE url=%s'
+		self.cursor.execute(sql, (url,))
+		result = self.cursor.fetchone()
+		return result
+
+	#JJ: getCamera(self, id)
+
+	#JJ: def getFound(self, slug):
+
+	#JJ: def saveRequest(self, request_data)
+
+	#JJ: def findCameraWithinBounds(self, bound_north, bound_south, bound_east, bound_west):
+
+	#JJ: def setFound(self, slug)
+
+	def animalFound(self, camera_id, animal_type, image):
+		sql = 'INSERT INTO foundanimal (camera_id, type, img) VALUES (%s, %s, %s)'
+		result = self.cursor.execute(sql, (camera_id, animal_type, image,))
+		self.db.commit()
+		return result
+
+	def findActiveRequests(self, camera_id, animal_type):
+		sql = 'SELECT r.id, r.name, r.phone, r.animal_type FROM request r INNER JOIN requestCamera rc ON r.id=rc.request_id WHERE r.found=false AND r.animal_type=%s AND rc.camera_id=%s'
+		self.cursor.execute(sql, (animal_type, camera_id,))
+		return self.results(self.cursor)
+
 
--- api/main.py
+++ api/main.py
@@ -12,10 +12,42 @@ app = Flask(__name__)
 #def validate_found(data):
 #	if not data.has_key(''):
 
+# CORS
+@app.before_request
+def option_autoreply():
+	""" Always reply 200 on OPTIONS request """
+	if request.method == 'OPTIONS':
+		resp = app.make_default_options_response()
+
+		headers = None
+		if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+			headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+		h = resp.headers
+
+		h['Access-Control-Allow-Origin'] = request.headers['Origin']
+		h['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS"
+		h['Access-Control-Max-Age'] = "10000"
+
+		if headers is not None:
+			h['Access-Control-Allow-Headers'] = headers
+
+		return resp
+
+@app.after_request
+def set_allow_origin(resp):
+	h = resp.headers
+	if request.method != 'OPTIONS' and 'Origin' in request.headers:
+		h['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+	return resp
+
+def errorResponse(code, msg):
+	return msg, code
 
 @app.route("/")
 def index():
-	return "Hello World!"
+	return "why u here?"
 
 @app.route("/cameras")
 def cameras():
@@ -24,14 +56,34 @@ def cameras():
 		result = json.dumps(data)
 		return Response(result, mimetype='text/json')
 
+#{'isDogFound': dog_found, 'isCatFound': cat_found, 'url': url, 'image': file_name}
 @app.route('/found', methods = ['POST'])
 def found():
 	data = request.get_json()
+	with DBase() as db:
+		camera_id = db.getCameraByUrl(data['url'])
+		if camera_id is None:
+			return errorResponse(400, "camera url is not valid")
+		camera_id = camera_id[0] # tuple to int
+
+		animal_type = 'cat'
+		if data['isDogFound']:
+			animal_type = 'dog'
+		image_url = data['image']
+
+		result = db.animalFound(camera_id, animal_type, image_url)
+
+		print(json.dumps(result))
+		if result == False:
+			return errorResponse(500, "ERROR PROCESSING REQUEST 1000")
 	
-	# Validation
-	validate_found(data)
-	#print(data)
-	return "Post World!" + str(data)
+		animal_requests = db.findActiveRequests(camera_id, animal_type)
+
+		#for animal_request in animal_requests:
+			#JJ: TODO: twilio
+		return json.dumps(animal_requests)
+
+
 
 print("starting program...")
 
